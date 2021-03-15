ad95ea020931095e515ab09c7bcd8be2dc54113c
Daehan <dh16931@gmail.com>
2021-03-15 14:28:23 +0900

    feat: post api

--- src/Component/MainTask/MainTask.js
+++ src/Component/MainTask/MainTask.js
@@ -1,12 +1,29 @@
+import React from "react";
+
+import CreateTask from "./CreateTask";
+
+import useFetch from "../../utils/hooks/useFetch";
 
 
 function MainTask() {
-    return (
-      <div>
-        <p>MainTask</p>
-      </div>
-    );
-  }
-  
-  export default MainTask;
+  const [data, loading] = useFetch("http://54.180.93.113:16980/api/tasks/");
+  return (
+    <div>
+      <p>MainTask</p>
+      <CreateTask />
+      {loading ? (
+          "Loading..."
+      ) : (
+          <ul>
+          {data && data.map(({ id, url, description }) => (
+              <li key={`url-${id}`}>
+                  <a href={url}>{description}</a>
+              </li>
+          ))}
+          </ul>
+      )}
+    </div>
+  );
+}
+export default MainTask;
   
