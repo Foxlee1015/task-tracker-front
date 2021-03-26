408d8c157aa72070ed3ba571f6b25130a8ccf18b
Daehan <dh16931@gmail.com>
2021-03-27 08:45:10 +0900

    feat: checkbox to apicall

--- src/Component/MainTask/ListTask.js
+++ src/Component/MainTask/ListTask.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import { DataGrid } from '@material-ui/data-grid';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Checkbox from '@material-ui/core/Checkbox';
 
 import useFetch from "../../utils/hooks/useFetch";
+import  { apiPostCall } from "../../utils/apicall";
+import AlertSnackbar from "../../Common/Feedback/AlertSnackbar";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -19,23 +22,49 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-function ListTask() {
-
-  return (
-    <div>
-      <DataTable />
-    </div>
-  );
-}
-export default ListTask;
-  
-
-function DataTable() {
+export default function ListTask() {
   const classes = useStyles();
   const [data, loading] = useFetch(`${process.env.REACT_APP_API_URL}/tasks/`, []);  
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+  const handleClickCheck = (row) => {
+    const checked = row.checked === 0 ? 1 : 0;
+    const endpoint = `tasks/${row.id}?checked=${checked}`;
+
+    const failCallback = () => {
+      setSnackbarOpen(true);
+    }
+    const responseCallback = () => {
+      console.log('refresh~')
+    }
+
+    apiPostCall({
+      endpoint,
+      responseCallback,
+      failCallback,
+    })
+  }
+
+  const handleCellClick = (cell) => {
+    if (cell.field === "checked") {
+      handleClickCheck(cell.row);
+    }
+  }
+
   const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'checked', headerName: 'Checked', width: 70 },
+    { field: 'checked', headerName: 'Checked', width: 70,
+      renderCell: (params) => {
+        return (
+          <Checkbox
+            checked={params.row.checked === 1 ? true : false}
+            color="primary"
+            inputProps={{ 'aria-label': 'secondary checkbox' }}
+            
+        />
+        )
+      }
+    },
     { field: 'title', headerName: 'Title', width: 130 },
     {
       field: 'text',
@@ -47,6 +76,7 @@ function DataTable() {
   ];
   return (
     <div className={classes.DataBox}>
+      <AlertSnackbar open={snackbarOpen} handleClose={setSnackbarOpen} severity="error" />
       <div className={classes.progressBox}>
         {loading && <CircularProgress />}
       </div>
@@ -56,7 +86,12 @@ function DataTable() {
           pageSize={5} 
           checkboxSelection
           onRowSelected={(row)=>{
-            console.log(row);}} />
+            console.log(row);}}
+          onCellClick={(row)=>{
+            console.log(row);
+            handleCellClick(row);
+          }} 
+            />
     </div>
   );
 }
