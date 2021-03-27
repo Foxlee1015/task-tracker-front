bd844f87b7e4f631527ec2bce9044019bbdfa246
daehan <dh16931@gmail.com>
2021-03-27 11:09:30 +0900

    fix: build error

--- src/Component/MainTask/ListTask.js
+++ src/Component/MainTask/ListTask.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import { DataGrid } from '@material-ui/data-grid';
 import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
